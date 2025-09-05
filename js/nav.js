dd1a02f44f4760a8e670a94a485d9fab6122cbda
Niurca Quirarte <niurcaq@umich.edu>
2025-09-05 16:08:08 -0400

    finished bio/top part of about page; need to work on links + resume on about page

--- js/nav.js
+++ js/nav.js
@@ -11,3 +11,43 @@ function listOpt() {
   menu.classList.toggle("show");
   menu.classList.toggle("hide");
 }
+
+const bios = [
+  `Hi, I'm a Computer Science Engineering student at the 👩‍🎓University of Michigan 
+   with a passion for creating technology that solves meaningful problems. 
+   Based in📍Chicago, IL, and open to relocation. Currently looking for 
+   <span class="emp">full-time</span> positions in <span class="emp">software engineering</span> or <span class="emp">full-stack development</span> 
+   where I can grow as a developer and contribute to impactful projects!`,
+
+
+  `My main areas of interest are <span class="emp">software engineering</span>, <span class="emp">full-stack development</span>, 
+  <span class="emp">UX design</span>, <span class="emp">data analytics</span>, 
+  <span class="emp">cybersecurity</span>, <span class="emp">AI/ML</span>, and <span class="emp">mobile app development</span>.`,
+
+
+  `Coding Languages: <span class="emp">Python</span>, <span class="emp">C/C++</span>, <span class="emp">HTML</span>, <span class="emp">CSS</span>, 
+  <span class="emp">JavaScript</span>, <span class="emp">SQL</span>, <span class="emp">Matlab</span><br>
+
+  Frameworks: <span class="emp">Vue</span>, <span class="emp">Flask</span>, <span class="emp">React</span>, <span class="emp">Django</span>, 
+  <span class="emp">Bootstrap</span><br>
+
+  Development Tools: <span class="emp">Linux</span>, <span class="emp">Git</span>, <span class="emp">AWS</span>, <span class="emp">Docker</span><br>
+
+  Other: <span class="emp">Regex</span>, <span class="emp">Figma</span>, <span class="emp">Google Colab</span>, <span class="emp">Google Workspace</span>, 
+  <span class="emp">Microsoft Office</span>, <span class="emp">Tableau</span><br>
+
+  Spoken Languages: <span class="emp">English</span>, <span class="emp">Spanish</span>`,
+
+
+  `Some <span class="emp">hobbies outside of coding</span> include traveling ✈️, listening to music 🎧, building Legos🧩, 
+   playing basketball 🏀, hanging out with friends👥, and watching movies📺!`
+];
+
+let index = 0;
+const bioText = document.getElementById("bio-text");
+const nextBtn = document.getElementById("next-btn");
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % bios.length;
+  bioText.innerHTML = bios[index]; // use innerHTML since text contains <br> and spans
+});
